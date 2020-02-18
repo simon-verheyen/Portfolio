240002454ba36332b5eac54395e0a7ec0af8592a
Simon Verheyen <siverheyen@gmail.com>
2020-02-18 13:10:28 +0100

    Creation data collection foundation

--- data-projects/gapminder-project/data_management.py
+++ data-projects/gapminder-project/data_management.py
@@ -1,5 +1,33 @@
 import pandas as pd
 
+education = {'primary_school_cost': 'data/education/expenditure_per_student_primary_percent_of_gdp_per_person.csv',
+             'primary_school_completion': 'data/education/primary_completion_rate_total_percent_of_relevant_age_group.csv'}
+
+health = {'sanitation_access': 'data/health/at_least_basic_sanitation_overall_access_percent.csv',
+          'water_access': 'data/health/at_least_basic_water_source_overall_access_percent.csv',
+          'life_expectancy': 'data/health/life_expectancy_years.csv'}
+
+population = {'child_mortality': 'data/population/child_mortality_0_5_year_olds_dying_per_1000_born.csv',
+              'ratio_child_and_elderly': 'data/population/children_and_elderly_per_100_adults.csv',
+              'children_per_mother': 'data/population/children_per_woman_total_fertility.csv',
+              'median_age': 'data/population/median_age_years.csv',
+              'population_density': 'data/population/population_density_per_square_kn.csv',
+              'population': 'data/population/population_total.csv'}
+
+wealth = {'av_age_billionaires': 'data/wealth/average_age_of_dollar_billionaires_years.csv',
+          'gdp_yearly_growth': 'data/wealth/gdp_total_yearly_growth.csv',
+          'income_per_person': 'data/wealth/income_per_person_gdppercapita_ppp_inflation_adjusted.csv',
+          'inflation': 'data/wealth/inflation_annual_percent.csv',
+          'people_in_poverty': 'data/wealth/number_of_people_in_poverty.csv',
+          'tax_perc_gdp': 'data/wealth/tax_revenue_percent_of_gdp.csv',
+          'amount_billionaires': 'data/wealth/total_number_of_dollar_billionaires.csv'}
+
+other = {'co2_emissions': 'data/other/co2_emissions_tonnes_per_person.csv',
+         'corruption_ind': 'data/other/corruption_perception_index_cpi.csv',
+         'hdi': 'data/other/hdi_human_development_index.csv'}
+
+data_locations = dict(education, **health, **population, **wealth, **other)
+
 
 def get_country_data(country, attr=[], from_param=1800, to=2018):
     attr = ['Year'] + attr
@@ -8,13 +36,12 @@ def get_country_data(country, attr=[], from_param=1800, to=2018):
         structure[0][1].append(x)
 
     for i in range(1, len(attr)):
-        path = 'data/'
-        filename = get_filename(attr[i])
-        if filename == '':
+        path = data_locations.get(attr[i], "wrong")
+
+        if path == "wrong":
             print("Bad attribute input: " + attr[i])
             return
 
-        path += filename
         df = pd.read_csv(path, index_col='country')
         attr_values = df.loc[country, [str(x) for x in range(from_param, to + 1)]].tolist()
 
@@ -30,7 +57,7 @@ def get_country_data(country, attr=[], from_param=1800, to=2018):
 
 
 def get_global_data(attr=[], from_param=1800, to=2018):
-    df_pop = pd.read_csv('data/population_total.csv', index_col='country')[[str(x) for x in range(from_param, to + 1)]]
+    df_pop = pd.read_csv('data/population/population_total.csv', index_col='country')[[str(x) for x in range(from_param, to + 1)]]
     total_pop_per_year = df_pop.sum(axis=0, skipna=True).tolist()
 
     attr = ['Year'] + attr
@@ -40,14 +67,12 @@ def get_global_data(attr=[], from_param=1800, to=2018):
         structure[0][1].append(x)
 
     for i in range(1, len(attr)):
-        path = 'data/'
-        filename = get_filename(attr[i])
+        path = data_locations.get(attr[i], "wrong")
 
-        if filename == '':
+        if path == "wrong":
             print("Bad attribute input: " + attr[i])
             return
 
-        path += filename
         df = pd.read_csv(path, index_col='country')[[str(x) for x in range(from_param, to + 1)]]
 
         df_rel = df * df_pop / total_pop_per_year
@@ -64,18 +89,14 @@ def get_global_data(attr=[], from_param=1800, to=2018):
     return new_df
 
 
-def get_filename(attr):
-    if attr == 'child_mortality':
-        filename = 'child_mortality_0_5_year_olds_dying_per_1000_born.csv'
-    elif attr == 'av_child_per_mother':
-        filename = 'children_per_woman_total_fertility.csv'
-    elif attr == 'income_per_person':
-        filename = 'income_per_person_gdppercapita_ppp_inflation_adjusted.csv'
-    elif attr == 'av_life_expectancy':
-        filename = 'life_expectancy_years.csv'
-    elif attr == 'population':
-        filename = 'population_total.csv'
-    else:
-        filename = ''
+def get_data(countries=[], attr=[], from_param=1800, to=2018):
+    Dict = {}
+    for country in countries:
+        if country == 'Global':
+            df = get_global_data(attr, from_param, to)
+        else:
+            df = get_country_data(country, attr, from_param, to)
 
-    return filename
+        Dict[df.name] = df
+
+    return Dict
--- data-projects/gapminder-project/my_tools.py
+++ data-projects/gapminder-project/my_tools.py
@@ -2,16 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_df_big(df):
+def plot_big(df,  title=''):
     plt.figure(figsize=(15, 10))
+    plt.suptitle(title)
 
     for col in df:
         plt.plot(df.index, df[col], label=col)
+    plt.title(df.name)
     plt.legend()
     plt.show()
 
 
-def plot_side_by_side(df1, df2, title):
+def plot_side_by_side(df1, df2, title=''):
     plt.figure(figsize=(15, 5))
     plt.suptitle(title)
 
@@ -29,8 +31,33 @@ def plot_side_by_side(df1, df2, title):
     plt.show()
 
 
-def normalize_columns(df):
+def plot_dict(Dict):
+    temp_name = ''
+    for entry in Dict:
+        if not temp_name:
+            temp_name = entry
+        else:
+            plot_side_by_side(Dict[temp_name], Dict[entry])
+            temp_name = ''
+
+    if temp_name:
+        plot_big(Dict[temp_name])
+
+
+def normalize_df(df):
     averages = np.sum(df) / len(df)
     new_df = df / averages
 
+    new_df.name = df.name
+
     return new_df
+
+
+def normalize_dict(Dict):
+    dict_norm = {}
+    for entry in Dict:
+        df_norm = normalize_df(Dict[entry])
+
+        dict_norm[df_norm.name] = df_norm
+
+    return dict_norm
